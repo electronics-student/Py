fd9b4949f59917d19607d5a7589fe08f37411c39
Yaz <electronics.beginner787@gmail.com>
2020-04-04 22:07:46 +0530

    Made a wrong comment symbol

--- pi.py
+++ pi.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 def main(msg):
-    // No comments needed 
+    # No comments needed
     print(msg)
 
-def secondary()
-	print("New fucntion") 
+def secondary():
+	print("New function")
 
 
 main("Hello Yaz")
+
+
 
 
